e10e5ca37fae96a9bf92b99e0656e93c48d3307b
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-08-08 20:32:22 +0700

    DER 1.2: fixed the ability to ascend beyond level 0 in randomized room setups (like Abyss)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/RuinsEntranceRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/RuinsEntranceRoom.java
@@ -23,7 +23,6 @@ package com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.entrance;
 
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
-import com.shatteredpixel.shatteredpixeldungeon.levels.features.LevelTransition;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.RuinsRoom;
 import com.watabou.utils.PathFinder;
@@ -79,6 +78,6 @@ public class RuinsEntranceRoom extends RuinsRoom {
 			Painter.set( level, entrance+i, Terrain.EMPTY );
 		}
 
-		level.transitions.add(new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE));
+		EntranceRoom.setupStairs(level, entrance);
 	}
 }
