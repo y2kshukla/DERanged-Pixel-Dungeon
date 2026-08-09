d53424a5935b0a5d70fa3214083bc47014467244
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-08-09 20:16:12 +0700

    DER 1.2: fixed unidentified items being unidentified in alchemy weapon hints

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/alchemy/AlchemyWeapon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/alchemy/AlchemyWeapon.java
@@ -12,9 +12,9 @@ public interface AlchemyWeapon {
 
     static String hintString(ArrayList<Class<?extends Item>> recipe) {
         if (recipe.get(1).isAssignableFrom(UpgradeDust.class)) {
-            return Messages.get(Item.class, "discover_hint_alchemy_one", Reflection.newInstance(recipe.get(0)).name());
+            return Messages.get(Item.class, "discover_hint_alchemy_one", Reflection.newInstance(recipe.get(0)).trueName());
         } else {
-            return Messages.get(Item.class, "discover_hint_alchemy_two", Reflection.newInstance(recipe.get(0)).name(), Reflection.newInstance(recipe.get(1)).name());
+            return Messages.get(Item.class, "discover_hint_alchemy_two", Reflection.newInstance(recipe.get(0)).trueName(), Reflection.newInstance(recipe.get(1)).trueName());
         }
     }
 
