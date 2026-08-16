daedd1b8fb0c6050f192e1127c87fddd5e99e57d
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2026-08-17 02:12:29 +0700

    DER 1.2: rat king talents now only appear with 33% chance in metamorph pool

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/exotic/ScrollOfMetamorphosis.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/exotic/ScrollOfMetamorphosis.java
@@ -236,6 +236,8 @@ public class ScrollOfMetamorphosis extends ExoticScroll {
 			Set<Talent> curTalentsAtTier = Dungeon.hero.talents.get(tier-1).keySet();
 
 			for (HeroClass cls : HeroClass.values()){
+				if (cls == HeroClass.RAT_KING && Random.Int(3) > 0)
+					continue;
 
 				ArrayList<LinkedHashMap<Talent, Integer>> clsTalents = new ArrayList<>();
 				Talent.initClassTalents(cls, clsTalents);
